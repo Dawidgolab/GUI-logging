88377ed0063950a81d4008bf110bde634b68c1f2
Dawidgolab <dawidg122@interia.pl>
2024-04-30 17:07:16 +0200

    Using get() function instead of the messagebox

--- Main.py
+++ Main.py
@@ -1,41 +1,63 @@
 from tkinter import *
 from tkinter import messagebox
 
+
+# Main functions
 window = Tk()
 window.title("Login form")
 window.geometry('340x440')
 window.config(bg = "#3d3d29")
-windologin = 'dawid'
-password = '12345'
 
-def correct_login():
-    messagebox.showinfo(title = "Login", message = "Login went through correctly !")
-def wrong_login():
-    messagebox.showinfo(title = "Login", message= "Incorrect login or password !")
+
+frame = Frame(bg="#3d3d29")
+
+
+
+
+
+
+
+def login_details():
+    username = "dawid"
+    password = "12345"
+    if usernameEntry.get() == username and passwordEntry.get() == password:
+        print("Successfully logged in!")
+    else:
+        print("Invalid login")
+
 
 
 # Creating widgets
-loginLabel = Label(window, text="Login",bg = "#3d3d29",fg = "white",font = ("Arial",30))
+loginLabel = Label(
+    frame, text="Login",bg = "#3d3d29",fg = "white",font = ("Arial",30))
 
-usernameLabel = Label(window, text="Username",bg = "#3d3d29",fg = "white",font = ("Arial",16))
-usernameEntry = Entry(window,font = ("Arial",16))
+usernameLabel = Label(
+    frame, text="Username",bg = "#3d3d29",fg = "white",font = ("Arial",16))
+usernameEntry = Entry(frame,font = ("Arial",16))
 
-passwordLabel = Label(window, text="Password",bg = "#3d3d29",fg = "white",font = ("Arial",16))
-passwordEntry = Entry(window, show="*",font = ("Arial",16))
+passwordLabel = Label(
+    frame, text="Password",bg = "#3d3d29",fg = "white",font = ("Arial",16))
+passwordEntry = Entry(frame, show="*",font = ("Arial",16))
 
-loginButton = Button(window, text="Login", command = wrong_login)
+loginButton = Button(
+    frame, text="Login", command= login_details)
 
-'''if usernameEntry == login and passwordEntry == password:
-    Button(window, command=correct_login).grid()
-else:
-    Button(window, command=wrong_login)'''
+
+
 
 # Placing widgets on the screen
-loginLabel.grid(row=0, column=0, columnspan=2, sticky= "news")
+loginLabel.grid(row=0, column=0, columnspan=2, sticky= "news", pady=40)
 usernameLabel.grid(row=1, column=0)  # You need to specify the row and column for each widget
-usernameEntry.grid(row=1, column=1)
+usernameEntry.grid(row=1, column=1, pady=20)
 passwordLabel.grid(row=2, column=0)
-passwordEntry.grid(row=2, column=1)
-loginButton.grid(row=3, column=0, columnspan=2)
+passwordEntry.grid(row=2, column=1,  pady=20)
+loginButton.grid(row=3, column=0, columnspan=2, pady= 30)
+
+
+
+
+
+frame.pack()
+
 
 window.mainloop()
